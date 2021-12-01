d3eee6f9ac38c0d386273b145f08c5a0aad2305d
Kehvarl <Kehvarl@Kehvarl.com>
2021-11-30 17:19:49 -0700

    Setup Daily Template Create Day 1 Folder Ready To Begin

--- day/pt1.py
+++ day/pt1.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    print("Main")
+
+
+with open("input.txt") as f:
+    content = f.readlines()
+
+for v1 in content:
+    for v2 in content:
+        if int(v1) + int(v2) == 2020:
+            print(v1, v2, (int(v1)*int(v2)))
