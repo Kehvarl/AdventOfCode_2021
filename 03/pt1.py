456997de1f801e50a1d176ef22f85e44dc0bc300
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-02 22:51:42 -0700

    Day 3 4723/5497

--- 03/pt1.py
+++ 03/pt1.py
@@ -1,10 +1,42 @@
 
+file = "test.txt"
+# file = "input.txt"
 
-with open("input.txt") as f:
+
+with open(file) as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+ones = {}
+count = {}
+
+for line in content:
+    line = [c for c in line.strip()]
+    for i in range(len(line)):
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
+        if i not in ones:
+            ones[i] = int(line[i])
+        else:
+            ones[i] += int(line[i])
+
+print(count)
+print(ones)
+gama = ""
+epsilon = ""
+for i in range(len(count)):
+    if ones[i] > count[i]//2:
+        gama += "1"
+        epsilon += "0"
+    else:
+        gama += "0"
+        epsilon += "1"
+
+print(gama)
+print(int(gama, 2))
+print(epsilon)
+print(int(epsilon, 2))
+
+print(int(gama, 2) * int(epsilon, 2))
--- 03/pt2.py
+++ 03/pt2.py
@@ -1,10 +1,45 @@
 
-with open("input.txt") as f:
+# file = "test.txt"
+file = "input.txt"
+
+with open(file) as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def more_ones(ones, position):
+    count = 0
+    for line in ones:
+        line = [int(c) for c in line.strip()]
+        count += line[position]
+    return count >= len(ones)/2
+
+
+def filter_ones(ones, position, value):
+    return [ele for ele in ones if int(ele[position]) == value]
+
+
+oxy_content = content.copy()
+for pos in range(len(oxy_content[0])-1):
+    val = 0
+    if more_ones(oxy_content, pos):
+        val = 1
+    oxy_content = filter_ones(oxy_content, pos, val)
+
+print(oxy_content)
+print(int(oxy_content[0].strip(), 2))
+
+
+co2_content = content.copy()
+for pos in range(len(co2_content[0])-1):
+    if(len(co2_content) == 1):
+        break
+    val = 0
+    if not more_ones(co2_content, pos):
+        val = 1
+    co2_content = filter_ones(co2_content, pos, val)
+
+print(co2_content)
+print(int(co2_content[0].strip(), 2))
+
+print(int(co2_content[0].strip(), 2) * int(oxy_content[0].strip(), 2))
