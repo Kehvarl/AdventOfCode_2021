48fcef3588b3b263d86c5f450eb025ba8d0ef2ca
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-04 22:55:23 -0700

    Day 5 1404 / 3918 (with library)

--- 05/pt1.py
+++ 05/pt1.py
@@ -4,7 +4,38 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+segments = []
+for line in content:
+    start, end = line.split(" -> ")
+    sx, sy = [int(i) for i in start.split(",")]
+    ex, ey = [int(i) for i in end.split(",")]
+    if sx == ex or sy == ey:
+        segments.append([sx, sy, ex, ey])
+
+grid = {}
+for line in segments:
+    sx, sy, ex, ey = line
+    if sx == ex:
+        s = min(sy, ey)
+        e = max(sy, ey)
+        for y in range(s, e+1):
+            if not grid.get((sx, y), None):
+                grid[(sx, y)] = 1
+            else:
+                grid[(sx, y)] += 1
+    elif sy == ey:
+        s = min(sx, ex)
+        e = max(sx, ex)
+        for x in range(s, e+1):
+            if not grid.get((x, sy), None):
+                grid[(x, sy)] = 1
+            else:
+                grid[(x, sy)] += 1
+
+sum = 0
+for i in grid:
+    if grid[i] > 1:
+        sum += 1
+
+print(sum)
--- 05/pt2.py
+++ 05/pt2.py
@@ -1,10 +1,34 @@
+from bresenham import bresenham
 
 with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+segments = []
+for line in content:
+    start, end = line.split(" -> ")
+    sx, sy = [int(i) for i in start.split(",")]
+    ex, ey = [int(i) for i in end.split(",")]
+    segments.append([sx, sy, ex, ey])
+
+
+grid = {}
+def plot(x, y):
+    if not grid.get((x, y), None):
+        grid[(x, y)] = 1
+    else:
+        grid[(x, y)] += 1
+
+
+for line in segments:
+    sx, sy, ex, ey = line
+    for cell in bresenham(sx, sy, ex, ey):
+        plot(cell[0], cell[1])
+
+sum = 0
+for i in grid:
+    if grid[i] > 1:
+        sum += 1
+
+print(sum)
