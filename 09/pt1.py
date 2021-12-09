def7d629f0119f36b57d8887eab17bdeb7203981
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-08 23:00:02 -0700

    Day 9 4508 / 4868

--- 09/pt1.py
+++ 09/pt1.py
@@ -5,7 +5,43 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+map = []
+for line in content:
+    map.append([int(x) for x in list(line.strip())])
+
+height = len(map)
+width = len(map[0])
+lows = []
+
+
+def check(x, y):
+    test = map[y][x]
+    if x > 0:
+        if map[y][x-1] <= test:
+            return False
+    if x < width-1:
+        if map[y][x+1] <= test:
+            return False
+    if y > 0:
+        if map[y-1][x] <= test:
+            return False
+    if y < height-1:
+        if map[y+1][x] <= test:
+            return False
+    return True
+
+
+for r in range(height):
+    for c in range(width):
+        if check(c, r):
+            lows.append((c, r, map[r][c]))
+
+# print(lows)
+
+risk = 0
+for point in lows:
+    x, y, r = point
+    risk += r + 1
+
+print(risk)
--- 09/pt2.py
+++ 09/pt2.py
@@ -6,7 +6,67 @@ with open("input.txt") as f:
     #content = [int(x) for x in f.readlines()]
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+map = []
+for line in content:
+    map.append([int(x) for x in list(line.strip())])
+
+height = len(map)
+width = len(map[0])
+lows = []
+
+
+def check(x, y):
+    test = map[y][x]
+    if x > 0:
+        if map[y][x-1] <= test:
+            return False
+    if x < width-1:
+        if map[y][x+1] <= test:
+            return False
+    if y > 0:
+        if map[y-1][x] <= test:
+            return False
+    if y < height-1:
+        if map[y+1][x] <= test:
+            return False
+    return True
+
+
+for r in range(height):
+    for c in range(width):
+        if check(c, r):
+            lows.append((c, r, map[r][c]))
+
+# print(lows)
+
+
+def get_val(x, y):
+    if 0 <= x < width and 0 <= y < height:
+        return map[y][x]
+    else:
+        return False
+
+
+basins = []
+sizes = []
+for x, y, v in lows:
+    basin = set()
+    basin.add((x, y))
+    testing = set()
+    testing.add((x, y))
+    while len(testing) > 0:
+        x1, y1 = testing.pop()
+        v1 = map[y1][x1]
+        for x2, y2 in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            v2 = get_val(x1+x2, y1+y2)
+            if v2 and v2 < 9 and v2 > v1 and (x1+x2, y1+y2) not in basin:
+                basin.add((x1+x2, y1+y2))
+                testing.add((x1+x2, y1+y2))
+    basins.append(basin)
+    sizes.append(len(basin))
+
+print(sorted(sizes, reverse=True)[:3])
+total = 1
+for s in sorted(sizes, reverse=True)[:3]:
+    total *= s
+print(total)
