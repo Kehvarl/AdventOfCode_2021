72c3b7fe89efeec8b7c07b1e6705235a565aa95a
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-10 22:43:32 -0700

    Day 11 3437 / 3074

--- 11/pt2.py
+++ 11/pt2.py
@@ -5,8 +5,61 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
+octopodes = {}
+for r, row in enumerate(content):
+    for c, col in enumerate((row.strip())):
+        octopodes[(r, c)] = int(col)
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+def flash(r, c):
+    for dr in [-1, 0, 1]:
+        for dc in [-1, 0, 1]:
+            if dr == dc == 0:
+                continue
+            if r + dr < 0 or r + dr > 9:
+                continue
+            if c + dc < 0 or c + dc > 9:
+                continue
+            octo = octopodes[(r + dr, c + dc)]
+            if octo > -1:
+                octo += 1
+            if octo > 9:
+                octopodes[(r + dr, c + dc)] = -1
+                flash(r + dr, c + dc)
+                octo = -1
+            octopodes[(r + dr, c + dc)] = octo
+
+
+def count_flashes():
+    flashes = 0
+    for r in range(10):
+        for c in range(10):
+            if octopodes[(r, c)] == -1:
+                flashes += 1
+                octopodes[(r, c)] = 0
+    return flashes
+
+def doflash():
+    for r in range(10):
+        for c in range(10):
+            octo = octopodes[(r,c)]
+            if octo > 9:
+                octopodes[(r, c)] = -1
+                flash(r, c)
+                octo = -1
+            octopodes[(r, c)] = octo
+
+
+def step():
+    for r in range(10):
+        for c in range(10):
+            octopodes[(r, c)] += 1
+    doflash()
+
+#f = 0
+turn = 0
+while count_flashes() < 100:
+    turn += 1
+    step()
+
+print(turn)
