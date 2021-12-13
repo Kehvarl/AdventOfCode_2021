347e81ce948ace60d0891b738c59d3fd55d010c4
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-12 22:38:42 -0700

    Day 13 4152 / 2975

--- 13/pt1.py
+++ 13/pt1.py
@@ -5,7 +5,75 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+max_x = 0
+max_y = 0
+sheet = defaultdict(int)
+folds = []
+
+folding = False
+for line in content:
+    if not folding:
+        if line.strip() == "":
+            folding = True
+            continue
+        x, y = line.strip().split(',')
+        x = int(x)
+        y = int(y)
+        sheet[x, y] = 1
+        if x > max_x:
+            max_x = x
+        if y > max_y:
+            max_y = y
+
+    else:
+        discard, fold = line.strip().split("fold along ")
+        a, v = fold.split('=')
+        folds.append((a, int(v)))
+
+
+def display():
+    for y in range(max_y + 1):
+        line = ""
+        for x in range(max_x + 1):
+            if sheet[(x, y)] > 0:
+                line += "#"
+            else:
+                line += "."
+        print(line)
+
+
+def count():
+    total = 0
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            if sheet[(x, y)] > 0:
+                total += 1
+    return total
+
+
+def fold_x(x):
+    global max_x
+    global sheet
+    for y in range(max_y + 1):
+        for i, x2 in enumerate(range(x, max_x + 1)):
+            sheet[(x2-(2*i), y)] += sheet[(x2, y)]
+    max_x = x - 1
+
+
+def fold_y(y):
+    global max_y
+    global sheet
+    for i, y2 in enumerate(range(y, max_y + 1)):
+        for x in range(max_x + 1):
+            sheet[(x, y2-(2*i))] += sheet[(x, y2)]
+    max_y = y -1
+
+
+#for a, v in folds:
+a,v = folds[0]
+if a == 'y':
+    fold_y(v)
+elif a == 'x':
+    fold_x(v)
+
+print(count())
