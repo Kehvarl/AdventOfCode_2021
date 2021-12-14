e9d4704a109e21f56bb71378204cef91b5c6e4b3
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-13 23:16:09 -0700

    Day 14 5691 / 3519

--- 14/pt1.py
+++ 14/pt1.py
@@ -1,11 +1,38 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 from pprint import pprint
 
 with open("input.txt") as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+template = list(content[0].strip())
+rules = []
+
+for rule in content[2::]:
+    pair, insert = rule.strip().split("->")
+    rules.append((pair.strip(), insert.strip()))
+
+
+def step(template):
+    new_template = []
+    for i in range(len(template) - 1):
+        pair = template[i:i + 2]
+        for rule, insert in rules:
+            if pair == list(rule):
+                new_template.append(template[i])
+                new_template.append(insert)
+                # new_template.append(template[i+1])
+                break
+    new_template.append(template[-1])
+
+    return new_template
+
+for r in range(10):
+    template = step(template)
+    print(len(template))
+
+elements = set(template)
+counts = defaultdict(int)
+totals = Counter(template)
+print(totals)
+print(totals.most_common())
+print(totals.most_common()[0][1] - totals.most_common()[-1][1])
--- 14/pt2.py
+++ 14/pt2.py
@@ -1,12 +1,42 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 from pprint import pprint
 
 with open("input.txt") as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+
+template = content[0].strip()
+rules = {}
+
+for rule in content[2::]:
+    pair, insert = rule.strip().split("->")
+    rules[pair.strip()] = insert.strip()
+
+pairs = defaultdict(int)
+for i in range(len(template) - 1):
+    pair = template[i:i + 2]
+    pairs[pair] += 1
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def step(pairs):
+    new_pairs = defaultdict(int)
+    for pair, cnt in pairs.items():
+        insert = rules[pair]
+        new_pairs[pair[0] + insert] += cnt
+        new_pairs[insert + pair[1]] += cnt
+
+    return new_pairs
+
+
+for r in range(40):
+    pairs = step(pairs)
+
+totals = Counter()
+for pair, cnt in pairs.items():
+    totals[pair[0]] += cnt
+    totals[pair[1]] += cnt
+totals[template[0]] += 1
+totals[template[-1]] += 1
+totals = Counter({elem: cnt // 2 for elem, cnt in totals.items()})
+print(totals)
+print(totals.most_common())
+print(totals.most_common()[0][1] - totals.most_common()[-1][1])
