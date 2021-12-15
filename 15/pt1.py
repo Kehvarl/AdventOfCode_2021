65ca97ac80d27955a0c180d854f955b94823c22b
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-14 23:15:24 -0700

    Day 15 Not Solved

--- 15/pt1.py
+++ 15/pt1.py
@@ -1,11 +1,53 @@
 from collections import defaultdict
 from pprint import pprint
 
-with open("input.txt") as f:
+with open("test.txt") as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+map = []
+for line in content:
+    l = [int(x) for x in list(line.strip())]
+    map.append(l)
+
+height = len(map)
+width = len(map[0])
+
+visited = set()
+queue = [(0, 0, 0)]
+destination = (width - 1, height - 1)
+pathcost = {}
+
+neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+def get_neighbors(x, y):
+    n = []
+    for dx, dy in neighbors:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < width and 0 <= ny < width:
+            n.append((nx, ny, map[ny][nx]))
+    return n
+
+
+while True:
+    cost, x, y = queue[0]
+    if (x, y) == destination:
+        print(cost)
+        break
+    queue = queue[1:]
+
+    for nx, ny, nc in get_neighbors(x, y):
+        newcost = cost + nc
+        if (nx, ny) in pathcost and pathcost[(nx, ny)] <= newcost:
+            continue
+        pathcost[(nx, ny)] = newcost
+        queue.append((newcost, ny, nx))
+    queue = sorted(queue)
+
+
+
+
+
+
