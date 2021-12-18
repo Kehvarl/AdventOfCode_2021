e77a6467bb63d2683bcfbb63e4ad86e079e35db4
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-17 22:54:40 -0700

    Day 17, wtelf

--- 18/pt1.py
+++ 18/pt1.py
@@ -1,11 +1,42 @@
 from collections import defaultdict
 from pprint import pprint
 
-with open("input.txt") as f:
+with open("test1.txt") as f:
     content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+def parse(s):
+    def parse_helper(level=0):
+        try:
+            token = next(tokens)
+        except StopIteration:
+            if level:
+                raise Exception('Missing close paren')
+            else:
+                return []
+        if token == ']':
+            if not level:
+                raise Exception('Missing open paren')
+            else:
+                return []
+        elif token == '[':
+            return [parse_helper(level+1)] + parse_helper(level)
+        elif token == ',':
+            return parse_helper(level)
+        else:
+            return [int(token)] + parse_helper(level)
+    tokens = iter(s)
+    return parse_helper()
+
+
+def explode(number, level=0):
+    a, b = number
+    if isinstance(a, list):
+        a = explode(a, level + 1)
+    if isinstance(b, list):
+        b = explode(b, level + 1)
+    return a, b
+
+
+sf = parse("[[[[[9,8],1],2],3],4]")
+print(explode(sf[0]))
