be289cc0569c61624c1ca312f90d3472bc21300e
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-19 23:21:46 -0700

    Day 20 1554 / 2574
    Cheaters Prosper

--- 20/pt1.py
+++ 20/pt1.py
@@ -5,7 +5,67 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+algo = [0 if x == '.' else 1 for x in list(content[0].strip())]
+
+image = defaultdict(int)
+
+row0 = 0
+col0 = 0
+width = len(content[2])
+height = len(content[2:])
+
+for r in range(len(content[2:])):
+    line = content[2+r].strip()
+    for c in range(len(line)):
+        image[(r, c)] = 0 if line[c] == '.' else 1
+
+
+def get_neighbors(r, c, image):
+    ret = ""
+    for nr in range(r-1, r+2):
+        for nc in range(c-1, c+2):
+            if nr < row0 or nr > height or nc < col0 or nc > width:
+                ret += str(0)
+            else:
+                ret += str(image[(nr, nc)])
+    return ret
+
+
+def get_pixel(val):
+    return algo[int(val, 2)]
+
+
+def step(image):
+    newimage = defaultdict(int)
+    for r in range(row0-3, height+4):
+        for c in range(col0-3, width+4):
+            newimage[(r, c)] = get_pixel(get_neighbors(r, c, image))
+    return newimage
+
+
+test = get_neighbors(2, 2, image)
+print(test, int(test, 2), get_pixel(test))
+
+image = step(image)
+row0 -= 3
+col0 -= 3
+height += 3
+width += 3
+image = step(image)
+
+
+# print(image)
+total = 0
+
+for r in range(row0+1, height):
+    line = ""
+    for c in range(col0+1, width):
+        total += image[(r, c)]
+        if image[(r, c)] == 1:
+            line += '#'
+        else:
+            line += '.'
+    print(line)
+
+print(total)
--- 20/pt2.py
+++ 20/pt2.py
@@ -6,7 +6,67 @@ with open("input.txt") as f:
     #content = [int(x) for x in f.readlines()]
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+algo = [0 if x == '.' else 1 for x in list(content[0].strip())]
+
+image = defaultdict(int)
+
+row0 = 0
+col0 = 0
+width = len(content[2])
+height = len(content[2:])
+
+for r in range(len(content[2:])):
+    line = content[2+r].strip()
+    for c in range(len(line)):
+        image[(r, c)] = 0 if line[c] == '.' else 1
+
+
+def get_neighbors(r, c, image, i):
+    ret = ""
+    for nr in range(r-1, r+2):
+        for nc in range(c-1, c+2):
+            if nr < row0 or nr > height or nc < col0 or nc > width:
+                if i % 2 == 0:
+                    ret += '1'
+                else:
+                    ret += '0'
+            else:
+                ret += str(image[(nr, nc)])
+    return ret
+
+
+def get_pixel(val):
+    return algo[int(val, 2)]
+
+
+def step(image, i):
+    newimage = defaultdict(int)
+    for r in range(row0-3, height+4):
+        for c in range(col0-3, width+4):
+            newimage[(r, c)] = get_pixel(get_neighbors(r, c, image, i))
+    return newimage
+
+
+for i in range(50):
+    print(i)
+    image = step(image, i)
+    row0 -= 1
+    col0 -= 1
+    height += 1
+    width += 1
+
+
+# print(image)
+total = 0
+
+for r in range(row0+1, height):
+    line = ""
+    for c in range(col0+1, width):
+        total += image[(r, c)]
+        if image[(r, c)] == 1:
+            line += '#'
+        else:
+            line += '.'
+    print(line)
+
+print(total)
