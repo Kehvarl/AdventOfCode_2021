e7109544165347c4d2fd1607b7b870a49ac0a4ab
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-20 22:40:45 -0700

    Day 21

--- 21/pt1.py
+++ 21/pt1.py
@@ -1,11 +1,48 @@
+import random
 from collections import defaultdict
 from pprint import pprint
 
-with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+player_1 = 8 # 4
+score_1 = 0
+player_2 = 3 # 8
+score_2 = 0
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def next_roll(r):
+    r += 1
+    if r == 101:
+        r = 1
+    return r
+
+def take_turn(player, start):
+    # roll = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+    start = next_roll(start)
+    roll = start
+    start = next_roll(start)
+    roll += start
+    start = next_roll(start)
+    roll += start
+
+    player = player + roll
+    while player > 10:
+        player -= 10
+    return player, start
+
+
+turn = 0
+start = 0
+rolls = 0
+while True:
+    turn += 1
+    player_1, start = take_turn(player_1, start)
+    score_1 += player_1
+    rolls += 3
+    if score_1 >= 1000:
+        break
+    player_2, start = take_turn(player_2, start)
+    score_2 += player_2
+    rolls += 3
+    if score_2 >= 1000:
+        break
+
+print(turn, rolls, player_1, score_1, player_2, score_2)
+print(min(score_1, score_2) * rolls)
--- 21/pt2.py
+++ 21/pt2.py
@@ -1,12 +1,29 @@
+import random
 from collections import defaultdict
 from pprint import pprint
 
-with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+Memo = {}
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def count_win(p1, p2, s1, s2):
+    if s1 >= 21:
+        return 1, 0
+    if s2 >= 21:
+        return 0, 1
+    if (p1, p2, s1, s2) in Memo:
+        return Memo[(p1, p2, s1, s2)]
+
+    win_counts = (0, 0)
+    for d1 in range(1, 4):
+        for d2 in range(1, 4):
+            for d3 in range(1, 4):
+                new_p1 = (p1 + d1 + d2 + d3) % 10
+                new_s1 = s1 + new_p1 + 1
+
+                x1, y1 = count_win(p2, new_p1, s2, new_s1)
+                win_counts = (win_counts[0] + y1, win_counts[1] + x1)
+    Memo[(p1, p2, s1, s2)] = win_counts
+    return win_counts
+
+
+print(max(count_win((8-1), (3-1), 0, 0)))
