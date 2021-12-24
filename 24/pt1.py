d220987b1443f9fc1425246dd9645f770070c5be
Kehvarl <Kehvarl@Kehvarl.com>
2021-12-23 23:32:48 -0700

    Day 24 WTELF

--- 24/pt1.py
+++ 24/pt1.py
@@ -5,7 +5,66 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+
+alu = []
+for line in content:
+    cmd = line.strip().split()
+    alu.append(cmd)
+
+
+def compute(alu, input):
+    state = defaultdict(int)
+    input = [int(x) for x in list(str(input))]
+    i = 0
+
+    for cmd in alu:
+        if cmd[0] == 'inp':
+            state[cmd[1]] += input[i]
+            i += 1
+        elif cmd[0] == 'add':
+            a = state[cmd[1]]
+            if cmd[2].isnumeric() or cmd[2][0] == '-':
+                b = int(cmd[2])
+            else:
+                b = state[cmd[2]]
+            state[cmd[1]] = a + b
+        elif cmd[0] == 'mul':
+            a = state[cmd[1]]
+            if cmd[2].isnumeric() or cmd[2][0] == '-':
+                b = int(cmd[2])
+            else:
+                b = state[cmd[2]]
+            state[cmd[1]] = a * b
+        elif cmd[0] == 'div':
+            a = state[cmd[1]]
+            if cmd[2].isnumeric() or cmd[2][0] == '-':
+                b = int(cmd[2])
+            else:
+                b = state[cmd[2]]
+            state[cmd[1]] = a // b
+        elif cmd[0] == 'mod':
+            a = state[cmd[1]]
+            if cmd[2].isnumeric() or cmd[2][0] == '-':
+                b = int(cmd[2])
+            else:
+                b = state[cmd[2]]
+            state[cmd[1]] = a % b
+        elif cmd[0] == 'eql':
+            a = state[cmd[1]]
+            if cmd[2].isnumeric() or cmd[2][0] == '-':
+                b = int(cmd[2])
+            else:
+                b = state[cmd[2]]
+            state[cmd[1]] = 1 if a == b else 0
+    return state
+
+
+sn = 98998519596997
+while True:
+    out = compute(alu, sn)
+    if out['z'] == 0:
+        print(sn, out)
+        break
+    sn -= 1
+    while '0' in str(sn):
+        sn -= 1
